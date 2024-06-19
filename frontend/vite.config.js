91d70ce139dad1327635acfd7eb68b737a3f0480
jessetfb <langatjesse@gmail.com>
2024-06-19 09:41:25 +0300

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     fs: {
       allow: [
-        // Allow serving files from the project directory
         '/Users/macbook/Documents/PHASE-3/FRONTEND/real-estate/frontend',
+        '/Users/macbook/Documents/challenges/APP/project-frontend/frontend/src',
+        '/Users/macbook/Documents/challenges/APP/project-frontend/frontend/node_modules', // Add node_modules directory
         // Add other necessary directories if required
       ]
     }
   }
-})
+});
