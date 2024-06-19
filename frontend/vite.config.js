59b3cad6b87fd04acf4eaf6826a4675ead213821
jessetfb <langatjesse@gmail.com>
2024-06-19 09:43:29 +0300

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
       allow: [
         '/Users/macbook/Documents/PHASE-3/FRONTEND/real-estate/frontend',
         '/Users/macbook/Documents/challenges/APP/project-frontend/frontend/src',
-        '/Users/macbook/Documents/challenges/APP/project-frontend/frontend/node_modules', // Add node_modules directory
+        '/Users/macbook/Documents/challenges/APP/project-frontend/frontend/node_modules',
+        '/Users/macbook/node_modules/react-multi-carousel' // Add the specific directory for react-multi-carousel
         // Add other necessary directories if required
       ]
     }
